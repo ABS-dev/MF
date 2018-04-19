375bd7e9528afdf9deddb9c8ec012068ba31e1dd
Marie Vendettuoli <mariecv26@gmail.com>
2018-04-19 14:41:15 -0700

    move examples into roxygen notation and roxygenize.

--- R/MFClusHier.R
+++ R/MFClusHier.R
@@ -1,18 +1,19 @@
-require(MF)
-require(CVBmisc)
-
-# data
-a <- data.frame(
-  room = paste('Room',rep(c('W','Z'),each=24)),
-  pen = paste('Pen',rep(LETTERS[1:6],each=8)),
-  litter = paste('Litter',rep(11:22,each=4)),
-  tx = rep(rep(c('vac','con'),each=2),12)
-)
-set.seed(76153)
-a$lung[a$tx=='vac'] <- rnorm(24,5,1.3)
-a$lung[a$tx=='con'] <- rnorm(24,7,1.3)
-
+#' @name MFh
+#' @title MFh
+#' @examples 
+#' a <- data.frame(
+#'  room = paste('Room',rep(c('W','Z'),each=24)),
+#'  pen = paste('Pen',rep(LETTERS[1:6],each=8)),
+#'  litter = paste('Litter',rep(11:22,each=4)),
+#'  tx = rep(rep(c('vac','con'),each=2),12)
+#'  )
+#' set.seed(76153)
+#' a$lung[a$tx=='vac'] <- rnorm(24,5,1.3)
+#' a$lung[a$tx=='con'] <- rnorm(24,7,1.3)
+#' 
+#' aCore <- MFh(lung ~ tx + room/pen/litter,a)
 # function - now it just returns the core table - all it needs are the ranks
+#' @export
 MFh <- function(formula, data, compare = c("con", "vac"), trace.it = FALSE){
   termlab <- attr(terms(formula), 'term.labels')
   tgroup <- termlab[1]
@@ -70,6 +71,47 @@ dMat <- function(x){
   return(a)
 }
 
+#' @name MFnest
+#' @title MFnest
+#' @examples
+#' 
+#' MFnest(aCore)
+#' MFnest(aCore, 'room')
+#' MFnest(aCore, 'pen')
+#' MFnest(aCore, 'litter')
+#' MFnest(aCore)
+#' #   level  N  U    MF
+#' # 1   All 48 45 0.875
+#' 
+#' MFnest(aCore, 'room')
+#' #    level  N  U        MF
+#' # 1 Room W 24 22 0.8333333
+#' # 2 Room Z 24 23 0.9166667
+#' 
+#' MFnest(aCore, 'pen')
+#' #   level N U   MF
+#' # 1 Pen A 8 6 0.50
+#' # 2 Pen B 8 8 1.00
+#' # 3 Pen C 8 8 1.00
+#' # 4 Pen D 8 7 0.75
+#' # 5 Pen E 8 8 1.00
+#' # 6 Pen F 8 8 1.00
+#' 
+#' MFnest(aCore, 'litter')
+#' #        level N U  MF
+#' # 1  Litter 11 4 4 1.0
+#' # 2  Litter 12 4 2 0.0
+#' # 3  Litter 13 4 4 1.0
+#' # 4  Litter 14 4 4 1.0
+#' # 5  Litter 15 4 4 1.0
+#' # 6  Litter 16 4 4 1.0
+#' # 7  Litter 17 4 4 1.0
+#' # 8  Litter 18 4 3 0.5
+#' # 9  Litter 19 4 4 1.0
+#' # 10 Litter 20 4 4 1.0
+#' # 11 Litter 21 4 4 1.0
+#' # 12 Litter 22 4 4 1.0
+#' @export
 # this does the summations on the core table
 MFnest <- function(Y, which.factor=NULL){
   if(is.null(which.factor)){
@@ -84,43 +126,3 @@ MFnest <- function(Y, which.factor=NULL){
   out$MF <- 2 * R - 1
   return(out)
 }
-
-aCore <- MFh(lung ~ tx + room/pen/litter,a)
-
-MFnest(aCore)
-MFnest(aCore, 'room')
-MFnest(aCore, 'pen')
-MFnest(aCore, 'litter')
-
-# > MFnest(aCore)
-#   level  N  U    MF
-# 1   All 48 45 0.875
-
-# > MFnest(aCore, 'room')
-#    level  N  U        MF
-# 1 Room W 24 22 0.8333333
-# 2 Room Z 24 23 0.9166667
-
-# > MFnest(aCore, 'pen')
-#   level N U   MF
-# 1 Pen A 8 6 0.50
-# 2 Pen B 8 8 1.00
-# 3 Pen C 8 8 1.00
-# 4 Pen D 8 7 0.75
-# 5 Pen E 8 8 1.00
-# 6 Pen F 8 8 1.00
-
-# > MFnest(aCore, 'litter')
-#        level N U  MF
-# 1  Litter 11 4 4 1.0
-# 2  Litter 12 4 2 0.0
-# 3  Litter 13 4 4 1.0
-# 4  Litter 14 4 4 1.0
-# 5  Litter 15 4 4 1.0
-# 6  Litter 16 4 4 1.0
-# 7  Litter 17 4 4 1.0
-# 8  Litter 18 4 3 0.5
-# 9  Litter 19 4 4 1.0
-# 10 Litter 20 4 4 1.0
-# 11 Litter 21 4 4 1.0
-# 12 Litter 22 4 4 1.0
