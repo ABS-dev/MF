1acae9f4321a50b3f03ee9e2c8e568da5c3f23ee
Marie Vendettuoli <mariecv26@gmail.com>
2018-08-01 11:25:11 -0500

    update test case

--- tests/testthat/test-hier.r
+++ tests/testthat/test-hier.r
@@ -15,11 +15,25 @@ nesteddf2 <- data.frame(room = rep(LETTERS[1:4], each = 8),
                         pen = rep(rep(1:2, each = 4), 4),
                         group = rep(rep(c('vac', 'con'), each = 2), 8), 
                         stringsAsFactors = FALSE)
-nesteddf2 <- nesteddf2[c(-3,-14, -29),]
 set.seed(2345)
-nesteddf2[nesteddf2$group == 'con', 'lesion'] <- rnorm(15, mean = 11, sd = 2)
+nesteddf2[nesteddf2$group == 'con', 'lesion'] <- rnorm(16, mean = 11, sd = 2)
 set.seed(5432)
-nesteddf2[nesteddf2$group == 'vac', 'lesion'] <- rnorm(14, mean = 10, sd = 1.5)                     
+nesteddf2[nesteddf2$group == 'vac', 'lesion'] <- rnorm(16, mean = 10, sd = 1.5)                     
+nesteddf2 <- nesteddf2[c(-3,-14, -29),]
+
+# three levels
+nesteddf3 <- data.frame(room = rep(LETTERS[1:4], each = 8),
+                        pen = rep(rep(1:2, each = 4), 4),
+                        litter = rep(c('123', '234', '345', '456', '567', '678', '789', '8910',
+                                       '321', '432', '543', '654', '765', '876', '987', '1098'),
+                                     2),
+                        group = rep(rep(c('vac', 'con'), each = 2), 8),
+                        stringsAsFactors = FALSE)
+set.seed(2345)
+nesteddf3[nesteddf3$group == 'con', 'lesion'] <- rnorm(16, mean = 11, sd = 2)
+set.seed(5432)
+nesteddf3[nesteddf3$group == 'vac', 'lesion'] <- rnorm(16, mean = 10, sd = 1.5)
+
 
 ## historical usage
 mfhist1 <- MFClus(lesion ~ group + cluster(litter), nesteddf1)
@@ -39,10 +53,14 @@ test_that("historical", {
 MFClusNested(lesion ~ group + cluster(litter), nesteddf1)
 MFClusNested(lesion ~ group + cluster(sex), nesteddf1)
 MFClusNested(lesion ~ group + cluster(litter/sex ), nesteddf1)
+
 MFClusNested(lesion ~ group + cluster(room), nesteddf2)
 MFClusNested(lesion ~ group + cluster(pen), nesteddf2)
 MFClusNested(lesion ~ group + cluster(room/pen ), nesteddf2)
 
-
+MFClusNested(lesion ~ group + cluster(room), nesteddf3)
+MFClusNested(lesion ~ group + cluster(pen), nesteddf3)
+MFClusNested(lesion ~ group + cluster(room/pen), nesteddf3)
+MFClusNested(lesion ~ group + cluster(room/pen/), nesteddf3)
 
 
