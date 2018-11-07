8470627b8f222d123185ae6235ce207da94ed4b4
mariev <mariecv26@gmail.com>
2018-11-07 07:34:34 -0800

    code formatting, readability

--- R/MFBoot.r
+++ R/MFBoot.r
@@ -1,11 +1,8 @@
-#' Estimates bootstrap confidence intervals for the mitigated fraction.
-#' 
-#' Resamples the data and produces bootstrap confidence intervals. Equal tailed 
-#' intervals are estimated by the 
-#' percentile method. Highest density intervals are estimated by selecting the 
-#' shortest of all possible intervals. 
-#' For BCa intervals, see Efron and Tibshirani section 14.3.
-#' 
+#' @description Estimates bootstrap confidence intervals for the mitigated fraction.
+#' @details Resamples the data and produces bootstrap confidence intervals. 
+#' Equal tailed intervals are estimated by the percentile method. Highest 
+#' density intervals are estimated by selecting the shortest of all possible 
+#' intervals. For BCa intervals, see Efron and Tibshirani section 14.3.
 #' @title Bootstrap MF CI
 #' @usage MFBoot(formula, data, compare = c("con", "vac"), b = 100, B = 100, 
 #'    alpha = 0.05, hpd = TRUE, bca = FALSE, return.boot = FALSE, trace.it = FALSE, 
@@ -41,95 +38,99 @@
 #' # observed median lower  upper
 #' # Equal Tailed        0.44 0.4496 0.152 0.7088
 #' # Highest Density     0.44 0.4496 0.152 0.7088
-##
-##--------------------------------------------------------------------
-## Bootstrap simple MF
-##--------------------------------------------------------------------
-MFBoot <- function(formula, data, compare = c("con", "vac"), b = 100, B = 100, 
-	alpha = 0.05, hpd = TRUE, bca = FALSE, return.boot = FALSE, trace.it = FALSE, seed = sample(1:100000, 1)){
-    # bootstrap confidence intervals for MF
-    # 11/17/99 initial coding
-    # 2/24/04 added BC.a interval
-    # 5/25/10 added empirical HPD interval
-    # takes b bootstrap samples B times, so nboot = B * b
-    # 10/1/2018 add seed utility
+#
+#--------------------------------------------------------------------
+# Bootstrap simple MF
+#--------------------------------------------------------------------
+MFBoot <- function(formula, data, compare = c("con", "vac"), b = 100, B = 100,
+	alpha = 0.05, hpd = TRUE, bca = FALSE, return.boot = FALSE, trace.it = FALSE,
+  seed = sample(1:100000, 1)){
+  # bootstrap confidence intervals for MF
+  # 11/17/99 initial coding
+  # 2/24/04 added BC.a interval
+  # 5/25/10 added empirical HPD interval
+  # takes b bootstrap samples B times, so nboot = B * b
+  # 10/1/2018 add seed utility
 
-    ## set seed
-    set.seed(seed)     
- 
-    A <- data.frame(model.frame(formula = formula, data = data))
-    resp <- A[, 1]
-    tx <- A[, 2]
-    x <- resp[tx == compare[1]]
-    y <- resp[tx == compare[2]]
-    
-    ## shortcircuit if complete separation
-    if(range(x)[1] < range(y)[1]){
-      lowtx <- x
-      hitx <- y
-    } else{
-      lowtx <- y
-      hitx <- x
-    }
-    
-    if(max(lowtx) < min(hitx)){
-      message('Complete separation observed. MF is 1.0 with no c.i.')
-      return()
-    }
+  # set seed
+  set.seed(seed)
 
-    rng <- 'Mersenne-Twister' 
-    RNGkind(rng)
-    nboot <- b * B
-    n.x <- length(x)
-    n.y <- length(y)
-    w <- function(xy, n.x){sum(rank(xy)[1:n.x])}
-    W <- rep(NA, b * B)
-    for(i in 1:B) {
-        x.b <- matrix(sample(x, size = b * n.x, replace = TRUE), b, n.x)
-        y.b <- matrix(sample(y, size = b * n.y, replace = TRUE), b, n.y)
-        W[(i - 1) * b + (1:b)] <- apply(cbind(x.b, y.b), 1, w, n.x)
-        if(trace.it){ cat("bootstrap samples", (i - 1) * b + 1, "to", 
-                          (i - 1) * b + b, "\n")}
+  A <- data.frame(model.frame(formula = formula, data = data))
+  resp <- A[, 1]
+  tx <- A[, 2]
+  x <- resp[tx == compare[1]]
+  y <- resp[tx == compare[2]]
+
+  # shortcircuit if complete separation
+  if (range(x)[1] < range(y)[1]) {
+    lowtx <- x
+    hitx <- y
+  } else {
+    lowtx <- y
+    hitx <- x
+  }
+
+  if (max(lowtx) < min(hitx)) {
+    message("Complete separation observed. MF is 1.0 with no c.i.")
+    return()
+  }
+
+  rng <- "Mersenne-Twister"
+  RNGkind(rng)
+  nboot <- b * B
+  n.x <- length(x)
+  n.y <- length(y)
+  w <- function(xy, n.x){
+    sum(rank(xy)[1:n.x])
+  }
+  W <- rep(NA, b * B)
+  for (i in 1:B) {
+    x.b <- matrix(sample(x, size = b * n.x, replace = TRUE), b, n.x)
+    y.b <- matrix(sample(y, size = b * n.y, replace = TRUE), b, n.y)
+    W[(i - 1) * b + (1:b)] <- apply(cbind(x.b, y.b), 1, w, n.x)
+    if (trace.it) {
+      cat("bootstrap samples", (i - 1) * b + 1, "to",
+                          (i - 1) * b + b, "\n")
     }
-    MF <- ((2 * W - n.x * (1 + n.x + n.y))/(n.x * n.y))
-    mf <- ((2 * w(c(x, y), n.x) - n.x * (1 + n.x + n.y))/(n.x * n.y))
-    
-    qprob <- c(0.5, alpha/2, 1 - alpha/2)
+  }
+  MF <- (2 * W - n.x * (1 + n.x + n.y)) / (n.x * n.y)
+  mf <- (2 * w(c(x, y), n.x) - n.x * (1 + n.x + n.y)) / (n.x * n.y)
+  qprob <- c(0.5, alpha / 2, 1 - alpha / 2)
+  qmf <- quantile(MF, prob = qprob)
+  stat <- matrix(c(mf, qmf), 1, 4, dimnames = list(c("Equal Tailed"),
+                                c("observed", "median", "lower", "upper")))
+  if (hpd) {
+    hpdmf <- emp.hpd(MF, alpha = alpha)
+    stat <- rbind(stat, "Highest Density" = c(mf, median(MF), hpdmf))
+  }
+  if (bca) {
+    z0 <- qnorm(sum(MF < mf) / (b * B))
+    xy <- c(x, y)
+    nx.i <- rep(n.x - (1:0), c(n.x, n.y))
+    ny.i <- rep(n.y - (0:1), c(n.x, n.y))
+    theta <- rep(NA, n.x + n.y)
+    for (i in 1:(n.x + n.y)) {
+		  theta[i] <- (2 * w(xy[-i], nx.i[i]) - nx.i[i] *
+		                (1 + nx.i[i] + ny.i[i])) / (nx.i[i] * ny.i[i])
+	  }
+    theta.hat <- mean(theta)
+    acc <- sum((theta.hat - theta)^3) / (6 * sum((theta.hat - theta)^2)^(3 / 2))
+    z1 <- qnorm(alpha / 2)
+    z2 <- qnorm(1 - alpha / 2)
+    a1 <- pnorm(z0 + (z0 + z1) / (1 - acc * (z0 + z1)))
+    a2 <- pnorm(z0 + (z0 + z2) / (1 - acc * (z0 + z2)))
+    a5 <- pnorm(z0 + (z0 + 0) / (1 - acc * (z0 + 0)))
+    qprob <- c(a5, a1, a2)
+    stuff <- c(acc, z0, a1, a2)
+    names(stuff) <- c("acc", "z0", "a1", "a2")
+    if (trace.it) {
+	    print(round(stuff, 4))
+    }
     qmf <- quantile(MF, prob = qprob)
-    stat <- matrix(c(mf, qmf), 1, 4, dimnames = list(c('Equal Tailed'), 
-                                  c("observed", "median", "lower", "upper")))
-    if(hpd){
-        hpdmf <- emp.hpd(MF, alpha=alpha)
-        stat <- rbind(stat, 'Highest Density' = c(mf, median(MF), hpdmf))
-    }
-    if(bca) {
-        z0 <- qnorm(sum(MF < mf)/(b * B))
-        xy <- c(x, y)
-        nx.i <- rep(n.x - (1:0), c(n.x, n.y))
-        ny.i <- rep(n.y - (0:1), c(n.x, n.y))
-        theta <- rep(NA, n.x + n.y)
-        for(i in 1:(n.x + n.y)){
-			theta[i] <- ((2 * w(xy[ - i], nx.i[i]) - nx.i[i] * 
-			                (1 + nx.i[i] + ny.i[i]))/(nx.i[i] * ny.i[i]))
-		}
-        theta.hat <- mean(theta)
-        acc <- sum((theta.hat - theta)^3)/(6 * sum((theta.hat - theta)^2)^(3/2))
-        z1 <- qnorm(alpha/2)
-        z2 <- qnorm(1 - alpha/2)
-        a1 <- pnorm(z0 + (z0 + z1)/(1 - acc * (z0 + z1)))
-        a2 <- pnorm(z0 + (z0 + z2)/(1 - acc * (z0 + z2)))
-        a5 <- pnorm(z0 + (z0 + 0)/(1 - acc * (z0 + 0)))
-        qprob <- c(a5, a1, a2)
-        stuff <- c(acc, z0, a1, a2)
-        names(stuff) <- c("acc", "z0", "a1", "a2")
-        if(trace.it){
-			print(round(stuff, 4))
-		}
-        qmf <- quantile(MF, prob = qprob)
-        stat <- rbind(stat, 'BC.a'= c(mf, qmf))
-    }
-    out <- mfboot$new(stat = stat, nboot = nboot, alpha = alpha, seed = seed, 
+    stat <- rbind(stat, "BC.a" = c(mf, qmf))
+  }
+  out <- mfboot$new(stat = stat, nboot = nboot, alpha = alpha, seed = seed,
 			rng = rng, compare = compare, sample = MF)
 
-    return(out)
+  return(out)
 }
